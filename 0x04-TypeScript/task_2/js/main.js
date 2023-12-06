1bf698487f65d5e4b28ef7be071c693069152566
cheronodaisy <jepchumbadaisy96@gmail.com>
2023-12-06 10:31:26 +0000

    task 2 updates

--- 0x04-TypeScript/task_2/js/main.js
+++ 0x04-TypeScript/task_2/js/main.js
@@ -1,20 +1,63 @@
-"use strict";
-exports.__esModule = true;
-var printTeacher = function (firstName, lastName) {
-    return firstName[0] + ". " + lastName;
-};
-exports.printTeacher = printTeacher;
-var StudentClass = /** @class */ (function () {
-    function StudentClass(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+var Director = /** @class */ (function () {
+    function Director() {
     }
-    StudentClass.prototype.workOnHomework = function () {
-        return "Currently working";
+    Director.prototype.workFromHome = function () {
+        return "Working from home";
     };
-    StudentClass.prototype.displayName = function () {
-        return this.firstName;
+    Director.prototype.getCoffeeBreak = function () {
+        return "Getting a coffee break";
     };
-    return StudentClass;
+    Director.prototype.workDirectorTasks = function () {
+        return "Getting to director tasks";
+    };
+    return Director;
 }());
-exports.StudentClass = StudentClass;
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return "Cannot work from home";
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return "Cannot have a break";
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return "Getting to work";
+    };
+    return Teacher;
+}());
+function createEmployee(salary) {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+}
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee("$500"));
+// functions specific to employees
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+function executeWork(employee) {
+    if (employee instanceof Director) {
+        return employee.workDirectorTasks();
+    }
+    else if (employee instanceof Teacher) {
+        return employee.workTeacherTasks();
+    }
+}
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+function teachClass(todayClass) {
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    }
+    else if (todayClass === "History") {
+        return "Teaching History";
+    }
+}
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,48 +1,83 @@
-interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    contract: boolean;
-    [propName: string]: any;
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
   }
   
-  interface Directors extends Teacher {
-    numberOfReports: number;
+  interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
   }
   
-  interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
-  }
-  
-  const printTeacher: printTeacherFunction = (firstName, lastName) => {
-    return `${firstName[0]}. ${lastName}`;
-  };
-  
-  interface StudentConstructor {
-    new (firstName: string, lastName: string): StudentClassInterface;
-  }
-  
-  interface StudentClassInterface {
-    workOnHomework(): string;
-    displayName(): string;
-  }
-  
-  const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
-    firstName: string;
-    lastName: string;
-  
-    constructor(firstName: string, lastName: string) {
-      this.firstName = firstName;
-      this.lastName = lastName;
+  class Director implements DirectorInterface {
+    workFromHome(): string {
+      return "Working from home";
     }
-    workOnHomework(): string {
-      return "Currently working";
-    }
-    displayName(): string {
-      return this.firstName;
-    }
-  };
   
-  export { printTeacher, StudentClass };
+    getCoffeeBreak(): string {
+      return "Getting a coffee break";
+    }
+  
+    workDirectorTasks(): string {
+      return "Getting to director tasks";
+    }
+  }
+  
+  class Teacher implements TeacherInterface {
+    workFromHome(): string {
+      return "Cannot work from home";
+    }
+  
+    getCoffeeBreak(): string {
+      return "Cannot have a break";
+    }
+  
+    workTeacherTasks(): string {
+      return "Getting to work";
+    }
+  }
+  
+  function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
+      return new Teacher();
+    } else {
+      return new Director();
+    }
+  }
+  
+  console.log(createEmployee(200));
+  console.log(createEmployee(1000));
+  console.log(createEmployee("$500"));
+  
+  // functions specific to employees
+  
+  function isDirector(employee: Teacher | Director): boolean {
+    return employee instanceof Director;
+  }
+  
+  function executeWork(employee: Teacher | Director): string {
+    if (employee instanceof Director) {
+      return employee.workDirectorTasks();
+    } else if (employee instanceof Teacher) {
+      return employee.workTeacherTasks();
+    }
+  }
+  
+  console.log(executeWork(createEmployee(200)));
+  console.log(executeWork(createEmployee(1000)));
+  
+  // String literal types
+  
+  type Subjects = "Math" | "History";
+  
+  function teachClass(todayClass: Subjects): string {
+    if (todayClass === "Math") {
+      return `Teaching Math`;
+    } else if (todayClass === "History") {
+      return `Teaching History`;
+    }
+  }
+  
+  console.log(teachClass("Math"));
+  console.log(teachClass("History"));
